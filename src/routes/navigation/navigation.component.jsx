75434eda8d9d2cada4ba726cba61784311e01c5b
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-16 15:54:35 +0545

    styling navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,12 @@
 import {Link, Outlet} from 'react-router-dom'
-
+import {ReactComponent as Logo} from '../../assets/logo.svg'
+import './navigation.styles.scss'
 const Navigation = () => {
     return (
         <>
             <div className='navigation'>
                 <Link to='/' className='logo-container'>
-                    <div>Logo</div>
+                    <Logo className='logo' />
                 </Link>
                 <div className='nav-links-container'>
                     <Link to='/shop' className='nav-link'>Shop</Link>

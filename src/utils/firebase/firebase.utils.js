8bdf365027a3dcd8cbaaf2cbfb8d7edce73e92c5
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-16 19:46:33 +0545

    created user documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import {initializeApp} from 'firebase/app'
 import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import {doc, getDoc, getFirestore, setDoc} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyAZVWfmwcFz5JdPa4_wd6krOuWxXJEqDxM',
@@ -19,3 +20,24 @@ provider.setCustomParameters({
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    const userSnapshot = await getDoc(userDocRef)
+    if (!userSnapshot.exists()) {
+        const {displayName, email} = userAuth
+        const createdAt = new Date()
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.messsage)
+        }
+        return userDocRef
+    }
+}

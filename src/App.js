cace21773271194b371456bbbc07bc7a17274f8d
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-16 10:59:52 +0545

    category item component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import './categories.styles.scss'
+import CategoryItem from './components/category-item/category-item.component'
 const App = () => {
   const categories = [
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
       title: 'Womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
       title: 'Mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     }
   ]
   return (
     <div className='categories-container'>
-      {categories.map(({id, title}) => (
-        <div key={id} className='category-container'>
-          <div className='category-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   )

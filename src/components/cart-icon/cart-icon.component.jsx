84d27bf2e8902108fbe6d038bdbfd0db1c06bcc9
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-23 18:35:09 +0545

    cart item designs

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
@@ -13,7 +13,7 @@ const CartIcon = () => {
       onClick={toggleIsCartOpen}
     >
       <ShoppingCartIcon className='cart-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };

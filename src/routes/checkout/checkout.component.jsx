7373ac192611428897cd010796714555c47998bf
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-25 19:38:23 +0545

    checkout page ongoing

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,26 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import './checkout.styles.scss';
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const { cartItems, addItemToCart } = useContext(CartContext);
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+              <span>decrement</span>
+              <span onClick={() => addItemToCart(cartItem)}>increment</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Checkout;

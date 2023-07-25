0ecb8fde68cc57e1bf048b521f03a08713e41002
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-25 20:06:57 +0545

    checkout item

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,24 @@
+import './checkout-item.styles.scss';
+
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className='checkout-item-container'>
+      <div className='image-container'>
+        <img
+          src={imageUrl}
+          alt={`${name}`}
+        />
+      </div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>
+        <div className='arrow'>&#10094;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow'>&#10095;</div>
+      </span>
+      <span className='price'>{price}</span>
+      <div className='remove-button'>&#10005;</div>
+    </div>
+  );
+};
+export default CheckoutItem;

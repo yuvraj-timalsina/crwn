8f566d088d5a8dcd0d14600686a76e605dc45151
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-26 17:09:25 +0545

    css fixes

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -13,15 +13,18 @@ const Category = () => {
     setProducts(categoriesMap[category]);
   }, [categoriesMap, category]);
   return (
-    <div className='category-container'>
-      {products &&
-        products.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-          />
-        ))}
-    </div>
+    <>
+      <h2 className='category-title'>{category.toUpperCase()}</h2>
+      <div className='category-container'>
+        {products &&
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))}
+      </div>
+    </>
   );
 };
 export default Category;

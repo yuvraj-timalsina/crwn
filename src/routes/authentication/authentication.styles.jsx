bf929345e07004e5387c26c69e221f87cf4053f6
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-26 17:57:39 +0545

    auth styled components

---
+++ src/routes/authentication/authentication.styles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const AuthenticationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 900px;
+  margin: 30px auto;
+`;
+
+export { AuthenticationContainer };

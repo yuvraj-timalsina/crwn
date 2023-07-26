b07be8b513b3b71f44c668cba9b0a5fb75abeaee
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-26 18:01:04 +0545

    refractor

--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
-const AuthenticationContainer = styled.div`
+export const AuthenticationContainer = styled.div`
   display: flex;
   justify-content: space-between;
   width: 900px;
   margin: 30px auto;
 `;
-
-export { AuthenticationContainer };

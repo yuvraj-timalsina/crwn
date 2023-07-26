a43aa6fb167176c9e5a651c38b7c47742f6c827b
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-26 17:52:37 +0545

    category component styled component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
 import { CategoriesContext } from '../../contexts/categories.context';
-import './category.styles.scss';
+import { CategoryContainer, CategoryTitle } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
@@ -12,10 +12,11 @@ const Category = () => {
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [categoriesMap, category]);
+
   return (
     <>
-      <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard
@@ -23,8 +24,9 @@ const Category = () => {
               product={product}
             />
           ))}
-      </div>
+      </CategoryContainer>
     </>
   );
 };
+
 export default Category;
